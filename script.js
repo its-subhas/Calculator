0273856e8bbc887b59644d9bd8312d518a283cd9
Subhas Biswas <148651262+its-subhas@users.noreply.github.com>
2025-02-23 20:30:15 +0530

    Update script.js
    Optimize Code.

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-let cd = '';
-document.querySelector('#display').value = cd;
+let cd = "";
+
+function updateValue() {
+  document.querySelector("#display").value = cd;
+}
+
+function emtyZero(a, b) {
+  if (cd === "" || cd === "0") {
+    cd = a;
+  } else {
+    cd += b;
+  }
+  updateValue();
+}
+
+function equal() {
+  if (cd === "") {
+    cd = "";
+  } else if (cd === eval(cd)) {
+    cd = eval(cd);
+  } else {
+    cd = eval(cd);
+  }
+  updateValue();
+}
+
+function dot() {
+  if (cd === "") {
+    cd = "0";
+  } else if (cd === "0" || cd === "0.") {
+    cd = "0.";
+  } else {
+    cd += ".";
+  }
+  updateValue();
+}
+
+function clearAll() {
+  cd = "";
+  updateValue();
+}
+
+function backspace() {
+  cd = cd.slice(0, cd.length - 1);
+  updateValue();
+}
